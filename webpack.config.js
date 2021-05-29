9521a32ba0d4a02419d4c3909a17355d1c166fd8
Vito <vito@vitosamson.com>
2021-05-29 14:09:59 -0400

    bump webpack, ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -2,50 +2,40 @@ const path = require('path');
 const fs = require('fs');
 const { execSync } = require('child_process');
 const webpack = require('webpack');
-const BabiliPlugin = require('babili-webpack-plugin');
 const isDevelopment = process.env.NODE_ENV === 'development';
 const devClientId = '733edtKvu6rRUw';
 
-const plugins = [
-  new webpack.DefinePlugin({
-    'process.env': {
-      NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
-    },
-  }),
-  new webpack.ProvidePlugin({
-    preact: 'preact',
-  }),
-];
+/**
+ * Copy the background script to the extension folder after webpack finishes bundling the content scripts.
+ */
+class BackgroundScriptCopyPlugin {
+  apply(compiler) {
+    compiler.hooks.afterEmit.tap('Copy background script', compilation => {
+      const clientId = process.env.CLIENT_ID || devClientId;
+      const backgroundScript = fs
+        .readFileSync(path.resolve('./src/background/background.js'))
+        .toString()
+        .replace('__CLIENT_ID__', clientId);
 
-if (!isDevelopment) {
-  plugins.push(new BabiliPlugin());
+      fs.writeFileSync(path.resolve('./extension/background.js'), backgroundScript);
+      console.log('updated background.js with client ID', clientId);
+
+      if (!isDevelopment) {
+        this.zipExtension();
+      }
+    });
+  }
+
+  zipExtension() {
+    const name = process.env.BROWSER;
+    const filename = `${name}.zip`;
+    execSync(`cd extension && zip ${filename} ./* && mv ${filename} ../build`);
+    console.log(`built ${name} extension: ${filename}`);
+  }
 }
-
-function zipExtension(name) {
-  const filename = `${name}.zip`;
-  execSync(`cd extension && zip ${filename} ./* && mv ${filename} ../build`);
-  console.log(`built ${name} extension: ${filename}`);
-}
-
-// copy the background script to the extension folder after webpack finishes bundling the content scripts
-plugins.push(function() {
-  this.plugin('done', () => {
-    const clientId = process.env.CLIENT_ID || devClientId;
-    const backgroundScript = fs
-      .readFileSync(path.resolve('./src/background/background.js'))
-      .toString()
-      .replace('__CLIENT_ID__', clientId);
-
-    fs.writeFileSync(path.resolve('./extension/background.js'), backgroundScript);
-    console.log('updated background.js with client ID', clientId);
-
-    if (!isDevelopment) {
-      zipExtension(process.env.BROWSER);
-    }
-  });
-});
 
 module.exports = {
+  mode: isDevelopment ? 'development' : 'production',
   devtool: isDevelopment ? 'source-map' : false,
   entry: './src/content/index.tsx',
   output: {
@@ -55,12 +45,22 @@ module.exports = {
   module: {
     rules: [
       {
-        use: 'awesome-typescript-loader',
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
     ],
   },
-  plugins,
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify(process.env.NODE_ENV || 'production'),
+      },
+    }),
+    new webpack.ProvidePlugin({
+      preact: 'preact',
+    }),
+    new BackgroundScriptCopyPlugin(),
+  ],
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.json'],
   },
